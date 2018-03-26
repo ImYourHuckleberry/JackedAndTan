a959241ce86e5debb56228e24a081f33829f14c4
Tyler Krings <w.tyler.krings@gmail.com>
2018-03-26 11:00:24 -0500

    fixed issues with bootstrap added remove added form fields

--- src/main/java/com/jackedandtan/JackedAndTan/controllers/JackedandTanController.java
+++ src/main/java/com/jackedandtan/JackedAndTan/controllers/JackedandTanController.java
@@ -8,12 +8,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 @Controller
 @RequestMapping("jackedandtan")
 public class JackedandTanController {
-    static ArrayList<String> lifts = new ArrayList<>();
+    static HashMap<String, String> lifts = new HashMap<>();
     // request path "/jackedandtan"
+
+
     @RequestMapping(value="" )
     public String index(Model model){
 
@@ -21,8 +24,11 @@ public class JackedandTanController {
         //will pass data from controller to view this time passing an array list
         model.addAttribute("title", "One Rep Max");
         return "templatesforjackedandtancontroller/index";
+        //maybeneed jackedandtancontroller we will see
         // return index refers to the template name we are rendering (index)
     }
+
+
     @RequestMapping(value="addlift", method = RequestMethod.GET)
     public String displayAddLiftForm(Model model){
         model.addAttribute("title", "Add Lift");
@@ -31,9 +37,26 @@ public class JackedandTanController {
 
     }
     @RequestMapping(value="addlift", method= RequestMethod.POST)
-    public String processAddForm(@RequestParam() String liftName) {
+    public String processAddForm(@RequestParam() String liftName, @RequestParam String liftWeight) {
         //RequestParam says it shoud look for the parameter with the same name found in addlift and insert
-        lifts.add(liftName);
+        lifts.put(liftName, liftWeight);
+
+        return "redirect:";
+    }
+
+    @RequestMapping(value = "remove", method = RequestMethod.GET)
+    public String displayRemoveLiftForm(Model model) {
+        model.addAttribute("lifts", lifts.keySet());
+        model.addAttribute("title", "Remove Lift");
+        return "templatesforjackedandtancontroller/remove";
+    }
+
+    @RequestMapping(value = "remove", method = RequestMethod.POST)
+    public String processRemoveLiftForm(@RequestParam ArrayList<String> lift) {
+
+        for (String aLift : lift) {
+            lifts.remove(aLift);
+        }
 
         return "redirect:";
     }
